ac6e96171934cfdf8aa67ef2bea38ca815949abd
ixoye215 <ixoye215@gmail.com>
2022-06-11 01:27:59 -0500

    cita v2

---
+++ script.js
@@ -0,0 +1,94 @@
+Swal.fire({
+    title: 'Me regustas',
+    confirmButtonText: 'Ok',
+}).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire({
+            title: 'Pero hace de cuenta que no te dije',
+            confirmButtonText: 'Ok',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: 'Poke me da verguenza',
+                    confirmButtonText: 'Ok',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Swal.fire({
+                            title: 'Ameno de que yo tambien te guste',
+                            confirmButtonText: 'Ok',
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                Swal.fire({
+                                    title: 'Ahi si se me va un pokito la vergüenzita',
+                                    confirmButtonText: 'Ok',
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        Swal.fire({
+                                            imageUrl: 'https://i.pinimg.com/550x/7a/30/6d/7a306d71b43368962f9a8315047ad56d.jpg',
+                                            imageHeight: 450,
+                                            imageAlt: 'Gatito esperando respuesta',
+                                            title: 'No querer tener una cita conmigo?',
+                                            confirmButtonText: 'Sii',
+                                        }).then((result) => {
+                                            if (result.isConfirmed) {
+                                                Swal.fire({
+                                                    title: 'Yo digo no más',
+                                                    confirmButtonText: 'Ok',
+                                                }).then((result) => {
+                                                    if (result.isConfirmed) {
+                                                        Swal.fire({
+                                                            title: 'Te la tiro',
+                                                            confirmButtonText: 'Ok',
+                                                        }).then((result) => {
+                                                            if (result.isConfirmed) {
+                                                                Swal.fire({
+                                                                    title: 'Si no queres',
+                                                                    confirmButtonText: 'Ok',
+                                                                }).then((result) => {
+                                                                    if (result.isConfirmed) {
+                                                                        Swal.fire({
+                                                                            title: 'Era un chiste',
+                                                                            confirmButtonText: 'Ok',
+                                                                        }).then((result) => {
+                                                                            if (result.isConfirmed) {
+                                                                                Swal.fire({
+                                                                                    title: 'Pero si queres',
+                                                                                    confirmButtonText: 'Ok',
+                                                                                }).then((result) => {
+                                                                                    if (result.isConfirmed) {
+                                                                                        Swal.fire({
+                                                                                            title: 'Capaz no era tan chiste',
+                                                                                            confirmButtonText: 'Ok',
+                                                                                        }).then((result) => {
+                                                                                            if (result.isConfirmed) {
+                                                                                                Swal.fire({
+                                                                                                    imageUrl: 'https://stickerly.pstatic.net/sticker_pack/chFOsiBEHmprfgMBqw/ADV6PP/3/68a464b2-ce4b-4f18-9c63-2f804358c9cf.png',
+                                                                                                    imageHeight: 450,
+                                                                                                    imageAlt: 'Flores',
+                                                                                                    showConfirmButton: false,
+                                                                                                    allowOutsideClick: false
+                                                                                                })
+                                                                                            }
+                                                                                        });
+                                                                                    }
+                                                                                });
+                                                                            }
+                                                                        });
+                                                                    }
+                                                                });
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                }); 
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
